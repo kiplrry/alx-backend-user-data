a93e7dedea3a69d1e9e58bd2b5136d1ba00b198e
kiplrry <kiplarry19@gmail.com>
2024-05-30 16:42:28 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,12 +17,14 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
-        """init the class"""
+        """init the class
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format the record"""
+        """Format the record
+        """
         mes = super().format(record)
         return filter_datum(self.fields, RedactingFormatter.REDACTION,
                             mes, RedactingFormatter.SEPARATOR)
@@ -45,7 +47,7 @@ def filter_datum(fields: List[str], redaction: str,
     """
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
+                         f' {f}={redaction}{separator}', message)
     return message
 
 # def get_logger() -> logging.Logger:
