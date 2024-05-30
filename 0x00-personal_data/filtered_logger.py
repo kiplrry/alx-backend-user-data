e3757b22a1714ae65d22f97e2df51643927e4986
kiplrry <kiplarry19@gmail.com>
2024-05-30 16:18:24 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,7 +31,7 @@ def filter_datum(fields: list[str, str], redaction: str, message: str,
                  separator: str) -> str:
     """returns the log message obfuscated:"""
     pattern = f"({'|'.join(map(re.escape, fields))})=[^{separator}]*"
-    return re.sub(pattern, lambda m: f" {m.group(1)}={redaction}", message)
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
 
 
 def get_logger() -> logging.Logger:
