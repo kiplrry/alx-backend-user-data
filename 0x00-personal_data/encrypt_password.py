e568b9ae3b75d824ab9572c1aba4f2eacf0f4250
kiplrry <kiplarry19@gmail.com>
2024-05-30 18:49:04 +0300

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Encrypting passwords """
+
+from typing import ByteString
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    expects one string argument name password
+    and returns a salted, hashed password, which is a byte string.
+    """
+    passw = password.encode()
+    hashpwd = bcrypt.hashpw(passw, bcrypt.gensalt())
+    return hashpwd
