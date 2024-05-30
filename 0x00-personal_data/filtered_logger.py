030fc895415471c706f6b393c07b68f530e8317c
kiplrry <kiplarry19@gmail.com>
2024-05-30 16:16:13 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,7 @@ import logging
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -19,12 +20,11 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         """Format the record"""
         mes = super().format(record)
-        return filter_datum(self.fields, RedactingFormatter.REDACTION, 
-                            mes, RedactingFormatter.SEPARATOR )
+        return filter_datum(self.fields, RedactingFormatter.REDACTION,
+                            mes, RedactingFormatter.SEPARATOR)
 
 
 def filter_datum(fields: list[str, str], redaction: str, message: str,
@@ -32,7 +32,6 @@ def filter_datum(fields: list[str, str], redaction: str, message: str,
     """returns the log message obfuscated:"""
     pattern = f"({'|'.join(map(re.escape, fields))})=[^{separator}]*"
     return re.sub(pattern, lambda m: f" {m.group(1)}={redaction}", message)
-
 
 
 def get_logger() -> logging.Logger:
