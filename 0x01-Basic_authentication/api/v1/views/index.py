2a6b28146eefefd34c47f3cda47dd45ba0391033
kiplrry <kiplarry19@gmail.com>
2024-06-03 22:14:22 +0300

    app

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauth() -> str:
     """GET api/v1/unauthorized
 
