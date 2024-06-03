948d842fb58a52a9401383d8fc2ef7e82075572c
kiplrry <kiplarry19@gmail.com>
2024-06-03 22:33:59 +0300

    fixes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,9 +6,11 @@ from flask import Request, request
 from typing import List, TypeVar
 User = TypeVar('User')
 
+
 class Auth:
     """Authorization Module
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth
 
