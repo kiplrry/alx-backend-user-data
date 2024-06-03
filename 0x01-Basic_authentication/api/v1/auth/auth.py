ba61e4a56232ae873f69aaab378e09a44e63d51c
kiplrry <kiplarry19@gmail.com>
2024-06-03 21:34:16 +0300

    all

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+"""Authorization module
+"""
+from flask import Request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth"""
+        if path is None or not excluded_paths:
+            return True
+
+        path_slashed = path if path.endswith('/') \
+            else path + '/'
+        path = path_slashed.removesuffix('/')
+
+        print(path, path_slashed)
+        if path_slashed in excluded_paths \
+                or path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request: Request = None) -> str:
+        """auth header"""
+        if request is None or \
+                request.authorization is None:
+            return None
+        return request.authorization
+
+    def current_user(self, request: Request = None) -> TypeVar('User'):
+        """return Current user"""
+        return None
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+"""
+Basic auth implementation
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth
+
+    :param Auth: An implementatin of Basic Authentication
+    :type Auth: Auth
+    """
