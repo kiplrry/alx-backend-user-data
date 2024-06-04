88e0c0d87eee46534914dbbdd3f2e689f0180b72
kiplrry <kiplarry19@gmail.com>
2024-06-04 16:51:10 +0300

    get

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic auth implementation
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -69,3 +71,30 @@ class BasicAuth(Auth):
         if len(arrs) != 2:
             return None, None
         return tuple(arrs)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """get user from the credentials provided
+
+        :param user_email: users email
+        :type self: str
+        :param user_pwd: users pass
+        :type self: str
+        :returns: User
+        """
+        if user_email is None or user_pwd is None\
+            or not isinstance(user_pwd, str)\
+                or not isinstance(user_email, str):
+            return None
+
+        users = None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
