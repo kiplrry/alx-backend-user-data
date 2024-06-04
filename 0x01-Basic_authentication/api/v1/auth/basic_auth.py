b5ce53e541adff7ab792f8ed27fa701e002a82af
kiplrry <kiplarry19@gmail.com>
2024-06-04 16:09:41 +0300

    extract

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,22 @@ class BasicAuth(Auth):
     :param Auth: An implementatin of Basic Authentication
     :type Auth: Auth
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extrac Basic auth
+
+        :param authorization_header: Auth header
+        :type authorization_header: str
+        :return: the header or None
+        :rtype: str
+        """
+        if authorization_header is None or\
+                not isinstance(authorization_header, str):
+            return None
+        arrs = authorization_header.split(' ')
+        if len(arrs) != 2:
+            return None
+        
+        if arrs[0] != 'Basic':
+            return None
+
+        return arrs[1]
