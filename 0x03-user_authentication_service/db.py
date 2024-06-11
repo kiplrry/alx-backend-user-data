33d60a8b2629e553b409aa33b5b320fc8aa1d613
kiplrry <kiplarry19@gmail.com>
2024-06-11 20:19:25 +0300

    a

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,10 +34,9 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """saves user to the db
         """
-        # if email is None or not isinstance(email, str)\
-        #         or hashed_password is None or\
-        #         not isinstance(hashed_password, str):
-        #     return
+        if email is None or not isinstance(email, str)\
+                or hashed_password is None:
+            return None
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
