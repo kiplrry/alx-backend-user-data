e0d6f83eec59979822244ea31f70c8468cb06aff
kiplrry <kiplarry19@gmail.com>
2024-06-11 19:38:41 +0300

    auth

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Auth implementation"""
+import bcrypt
+from db import DB
+from typing import TypeVar
+from sqlalchemy.exc import NoResultFound
+User = TypeVar('User')
+
+
+def _hash_password(password: str) -> bytes:
+    """hashes a password"""
+    if password is None or not isinstance(password, str):
+        return
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user"""
+        if email is None or not isinstance(email, str) or\
+                password is None or not isinstance(password, str):
+            return
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed = _hash_password(password)
+        user = self._db.add_user(email, hashed)
+        return user
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""user model implementaion with sqlalchemy
+"""
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import String, Integer, Column
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class
+
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+
+    def __init__(self, email, hashed_password, session_id=None,
+                 reset_token=None):
+        self.email = email
+        self.hashed_password = hashed_password
+        self.session_id = session_id
+        self.reset_token = reset_token
