9a0f1156b9f08da6b947b1e048cb6c9e9e7c6170
kiplrry <kiplarry19@gmail.com>
2024-06-11 21:45:05 +0300

    a

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Auth implementation"""
 import bcrypt
-from db import DB
-from typing import TypeVar
-from sqlalchemy.exc import NoResultFound
-User = TypeVar('User')
+# from db import DB
+# from typing import TypeVar
+# from sqlalchemy.exc import NoResultFound
+# User = TypeVar('User')
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,22 +13,22 @@ def _hash_password(password: str) -> bytes:
     return hashed
 
 
-class Auth:
-    """Auth class to interact with the authentication database.
-    """
+# class Auth:
+#     """Auth class to interact with the authentication database.
+#     """
 
-    def __init__(self):
-        self._db = DB()
+#     def __init__(self):
+#         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> User:
-        """Registers a user"""
-        if email is None or not isinstance(email, str) or\
-                password is None or not isinstance(password, str):
-            return
-        try:
-            user = self._db.find_user_by(email=email)
-            raise ValueError(f'User {email} already exists')
-        except NoResultFound:
-            hashed = _hash_password(password)
-        user = self._db.add_user(email, hashed)
-        return user
+#     def register_user(self, email: str, password: str) -> User:
+#         """Registers a user"""
+#         if email is None or not isinstance(email, str) or\
+#                 password is None or not isinstance(password, str):
+#             return
+#         try:
+#             user = self._db.find_user_by(email=email)
+#             raise ValueError(f'User {email} already exists')
+#         except NoResultFound:
+#             hashed = _hash_password(password)
+#         user = self._db.add_user(email, hashed)
+#         return user
