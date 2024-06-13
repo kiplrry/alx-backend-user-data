87445fc93242cf8f10b003d0b97a046dc8f62ac9
kiplrry <kiplarry19@gmail.com>
2024-06-13 23:33:34 +0300

    ll

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,7 +87,7 @@ def update_password():
         AUTH.update_password(reset_token, password)
     except Exception:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"}), 200
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
